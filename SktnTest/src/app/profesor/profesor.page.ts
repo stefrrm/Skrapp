e841bc61ec6ffc7e11a4b2c6c07538ef31e8d6e4
stefano <ste.caceres@duocuc.cl>
2024-10-18 02:52:30 -0300

    Añadida version 1 de pagina profesor

--- SktnTest/src/app/profesor/profesor.page.ts
+++ SktnTest/src/app/profesor/profesor.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-profesor',
@@ -7,7 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfesorPage implements OnInit {
 
-  constructor() { }
+  now = new Date();
+
+  user = '';
+  fecha = this.now.toLocaleString();
+
+  cursos = [
+    { id: 1, nombre: 'Aplicaciones Moviles', codigo: 'INF666', seccion: '033V' },
+    { id: 2, nombre: 'Arquitectura', codigo: 'INF667', seccion: '014v' },
+    { id: 3, nombre: 'Machine Learning', codigo: 'INF668', seccion: '055D' },
+  ];
+
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    this.activeroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+        this.user = this.router.getCurrentNavigation()?.extras.state?.['id'];
+        console.log(this.router.getCurrentNavigation()?.extras.state?.['id']);
+      }
+    })
+  }
 
   ngOnInit() {
   }
